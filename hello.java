0936a4586de6d474530618c50284716539d5c9c8
kavsarg <kavsarg@gmail.com>
2021-10-14 20:41:12 -0700

    Initial commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+System.out.print("Hey,");
+System.out.println("How is your day?");
+System.out.println("xapkat said\"Hi\"to you");
+	}
+
+}
